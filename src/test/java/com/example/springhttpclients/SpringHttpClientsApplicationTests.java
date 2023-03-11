49fac641f262e562d311ec85d0e84e0a6c680b4c
kenokim <keno1413@gmail.com>
2023-03-11 21:23:08 +0900

    Initial commit

---
+++ src/test/java/com/example/springhttpclients/SpringHttpClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhttpclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHttpClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
